3ffde0122489c75197b54e01329279d4a9a17f63
Leducanh30112006 <le1duc2anh3@gmail.com>
2026-05-24 21:47:07 +0700

    Update BalanceValidationAspect.java

--- src/main/java/com/ra/ptit_cntt2_it211_ss8_ex5/aspect/BalanceValidationAspect.java
+++ src/main/java/com/ra/ptit_cntt2_it211_ss8_ex5/aspect/BalanceValidationAspect.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(3) // Chạy thứ ba (Chỉ chạy khi thời gian đã hợp lệ, để tránh query DB lãng phí)
+@Order(3) 
 @RequiredArgsConstructor
 public class BalanceValidationAspect {
 
